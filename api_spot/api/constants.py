d4fde875b12b91332021d87c4631b5da4e0f9b7b
Арсений Изимов <arsenyxiii@gmail.com>
2023-09-18 17:28:46 +0300

    изменил время и добавил в constants

--- api_spot/api/constants.py
+++ api_spot/api/constants.py
@@ -2,3 +2,4 @@ SUBJECT_EMAIL_REGISTRATION = 'Подтверждение электронной 
 REGISTRATION_TEMPLATE = 'activation.html'
 SUBJECT_EMAIL_FINISH_ACTIOVATION = 'Успешная регистрация'
 FINISH_ACTIOVATION_EMAIL = 'finish_activation.html'
+TIME_CHANGE_STATUS = 60 * 10
--- api_spot/api/tasks.py
+++ api_spot/api/tasks.py
@@ -4,12 +4,13 @@ from time import sleep
 from celery import shared_task
 
 from spots.models.order import Order, LOCK, PAID
+from api.constants import TIME_CHANGE_STATUS
 
 
 @shared_task()
 def change_status_task(order_id):
-    """Таска изменения статуса после 60*1 секнуд."""
-    sleep(60 * 1)  # Simulate expensive operation(s) that freeze Django
+    """Таска изменения статуса после n секнуд."""
+    sleep(TIME_CHANGE_STATUS)
     order = get_object_or_404(Order, pk=order_id)
     if order.status != PAID:
         order.status = LOCK
