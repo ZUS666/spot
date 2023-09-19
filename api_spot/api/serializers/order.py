ea69fbabc61dfc867ac0a878831e92827e0138e1
Арсений Изимов <arsenyxiii@gmail.com>
2023-09-19 23:52:48 +0300

    первые наброски

--- api_spot/api/serializers/order.py
+++ api_spot/api/serializers/order.py
@@ -3,6 +3,7 @@ from rest_framework.relations import StringRelatedField
 
 from api.fields import GetSpot
 from spots.models.order import Order
+from spots.constants import TIME_CHOICES
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -14,40 +15,33 @@ class OrderSerializer(serializers.ModelSerializer):
         default=GetSpot()
     )
     spot_name = StringRelatedField(source='spot.name', read_only=True)
-    first_name = StringRelatedField(source='user.first_name', read_only=True)
-    last_name = StringRelatedField(source='user.last_name', read_only=True)
-    duration = serializers.SerializerMethodField(read_only=True)
+    time = serializers.MultipleChoiceField(choices=TIME_CHOICES)
 
     class Meta:
         """Класс мета для модели Order."""
         model = Order
         fields = (
-            'spot', 'user', 'spot_name', 'first_name', 'last_name',
-            'start_date', 'end_date', 'duration'
+            'spot', 'user', 'spot_name',
+            'date', 'time', 'bill'
         )
 
-    def get_duration(self, obj):
-        """Получение продолжительность брони."""
-        time = obj.end_date - obj.start_date
-        return f'{round((time.total_seconds() / 3600), 3)} в часах'
+    # def validate(self, data):
+    #     """Проверка на пересечение с другими бронями."""
+    #     spot = data.get('spot')
+    #     start_date = data.get('start_date')
+    #     end_date = data.get('end_date')
 
-    def validate(self, data):
-        """Проверка на пересечение с другими бронями."""
-        spot = data.get('spot')
-        start_date = data.get('start_date')
-        end_date = data.get('end_date')
+    #     if end_date < start_date:
+    #         raise serializers.ValidationError(
+    #             {'start_date': 'Начало брони позже конца'})
 
-        if end_date < start_date:
-            raise serializers.ValidationError(
-                {'start_date': 'Начало брони позже конца'})
-
-        qs = Order.objects.filter(
-            spot=spot,
-            start_date__lt=end_date,
-            end_date__gt=start_date
-        )
-        if qs.exists():
-            raise serializers.ValidationError({
-                'Spot': 'Данный коворкинг уже забронирован',
-            })
-        return data
+    #     qs = Order.objects.filter(
+    #         spot=spot,
+    #         start_date__lt=end_date,
+    #         end_date__gt=start_date
+    #     )
+    #     if qs.exists():
+    #         raise serializers.ValidationError({
+    #             'Spot': 'Данный коворкинг уже забронирован',
+    #         })
+    #     return data
--- api_spot/api/tasks.py
+++ api_spot/api/tasks.py
@@ -1,17 +1,14 @@
 from django.shortcuts import get_object_or_404
 
-from time import sleep
 from celery import shared_task
 
 from spots.models.order import Order
 from spots.constants import NOT_PAID, PAID
-from django.conf import settings
 
 
 @shared_task()
 def change_status_task(order_id):
     """Таска изменения статуса после n секнуд."""
-    sleep(settings.TIME_CHANGE_STATUS)
     order = get_object_or_404(Order, pk=order_id)
     if order.status != PAID:
         order.status = NOT_PAID
