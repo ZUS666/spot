a6e6408bcf20eb179cd4496be03ab7d217f5fa39
ZUS666 <t4g1.86@gmail.com>
2023-09-23 02:39:52 +0500

    add fields

--- api_spot/api/serializers/locations.py
+++ api_spot/api/serializers/locations.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from spots.constants import MEETING_ROOM, WORK_SPACE
 from spots.models import Location
 from .extra_photo import ExtraPhotoGetSerializer
 
@@ -13,6 +14,10 @@ class LocationsGetSerializer(serializers.ModelSerializer):
         read_only=True,
         source='location_extra_photo'
     )
+    count_workspace = serializers.SerializerMethodField()
+    count_meeting_room = serializers.SerializerMethodField()
+    is_favorited = serializers.SerializerMethodField()
+    
 
     class Meta:
         model = Location
@@ -20,9 +25,24 @@ class LocationsGetSerializer(serializers.ModelSerializer):
             'name',
             'street',
             'house_number',
+            'open_time',
+            'close_time',
             'latitude',
             'longitude',
             'plan_photo',
             'extra_photo',
             'description',
         )
+
+    def get_is_favorited(self, instance, *args, **kwargs):
+        user = self.context['request'].user
+        if not user.is_authenticated:
+            return False
+        return instance.favorites.filter(user_id=user.id).exists()
+    
+    def get_count_workspace(self, instance, *args, **kwargs):
+        return instance.spots.filter(category=WORK_SPACE).count()
+        
+
+    def get_count_meeting_room(self, instance, *args, **kwargs):
+        return instance.spots.filter(category=MEETING_ROOM).count()
