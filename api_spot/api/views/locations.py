4a373bd2d5a864c1e0a7bb5224fd1d5a0235833d
Арсений Изимов <arsenyxiii@gmail.com>
2023-09-25 22:09:34 +0300

    new file:   api_spot/api/filters.py 	modified:   api_spot/api/views/locations.py 	modified:   api_spot/api_spot/settings.py

--- api_spot/api/views/locations.py
+++ api_spot/api/views/locations.py
@@ -1,11 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
-from rest_framework import filters
 
 from api.mixins import RetrieveListViewSet
 from ..serializers import LocationsGetSerializer
 from spots.models import Location
+from api.filters import LocationFilter
 
 
 class LocationViewSet(RetrieveListViewSet):
@@ -15,6 +15,6 @@ class LocationViewSet(RetrieveListViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationsGetSerializer
     permission_classes = (AllowAny,)
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-    search_fields = ('name',)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = LocationFilter
     pagination_class = PageNumberPagination
