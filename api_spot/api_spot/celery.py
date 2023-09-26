3a2fbd15575e8b19adaaeb4c8535308a8af4886b
arseny_wsl <arsenyxiii@gmail.com>
2023-09-26 11:51:45 +0300

    исрпавил запуск локально

--- api_spot/api_spot/celery.py
+++ api_spot/api_spot/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 app_name = "api_spot"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{app_name}.settings")
-app = Celery(app_name, broker='redis://redis:6379/0')
+app = Celery(app_name, broker=os.getenv('CELERY_BROKER')) # broker='redis://redis:6379/0'
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
