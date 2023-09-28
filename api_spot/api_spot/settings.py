bf9d0f6586d6df3f5498a7b2e528b7c45089397a
Арсений Изимов <arsenyxiii@gmail.com>
2023-09-28 20:07:45 +0300

    modified:   api_spot/api_spot/settings.py 	modified:   api_spot/api_spot/urls.py

--- api_spot/api_spot/settings.py
+++ api_spot/api_spot/settings.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='default_key')
 
--- api_spot/api_spot/urls.py
+++ api_spot/api_spot/urls.py
@@ -5,7 +5,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'), 
+    path('api/docs/', SpectacularSwaggerView.as_view(
+        url_name='schema'), name='docs'
+    ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
