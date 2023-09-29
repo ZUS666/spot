c1ea83c72c159e7a6b0416c3be69aa0d523b8347
ZUS666 <t4g1.86@gmail.com>
2023-09-29 23:20:37 +0500

    test

--- api_spot/api_spot/settings.py
+++ api_spot/api_spot/settings.py
@@ -144,7 +144,7 @@ TIMEOUT_CACHED_LOW_PRICE = 3 * 60 * 60
 TIME_CHANGE_STATUS = 60 * 10
 LEN_CONFIRMATION_CODE = 6
 
-COMPANY_NAME = os.getenv('COMPANY_NAME', default='Beckend')
+COMPANY_NAME = os.getenv('COMPANY_NAME', default='Backend')
 
 EMAIL_BACKEND = 'gmailapi_backend.mail.GmailBackend'
 
