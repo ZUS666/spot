09f674c02dabfe1e2e2730ba0abb9f66edfe4122
arseny_wsl <arsenyxiii@gmail.com>
2023-10-02 11:36:21 +0300

    fix valid

--- api_spot/api/views/pay.py
+++ api_spot/api/views/pay.py
@@ -24,19 +24,19 @@ class PayView(APIView):
             data=request.data,
             context={'order_id': order_id}
         )
-        if serializer.is_valid(raise_exception=True):
-            order = serializer.validated_data.get('order')
-            self.check_object_permissions(request, order)
-            if order.status != WAIT_PAY:
-                return Response(
-                    'Можно оплачить только заказы со статусом '
-                    '"ожидается оплата"',
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            order.status = PAID
-            order.save()
-            order_finished_email(order)
+        serializer.is_valid(raise_exception=True)
+        order = serializer.validated_data.get('order')
+        self.check_object_permissions(request, order)
+        if order.status != WAIT_PAY:
             return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
+                'Можно оплачить только заказы со статусом '
+                '"ожидается оплата"',
+                status=status.HTTP_400_BAD_REQUEST
             )
+        order.status = PAID
+        order.save()
+        order_finished_email(order)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
