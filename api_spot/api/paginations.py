1da09381196921f282de42fedb8ffe0ade2e449c
ZUS666 <t4g1.86@gmail.com>
2023-10-11 12:38:19 +0500

    paginations

--- api_spot/api/paginations.py
+++ api_spot/api/paginations.py
@@ -7,3 +7,7 @@ class SixPageNumberPagination(PageNumberPagination):
 
 class ThreePageNumberPagination(PageNumberPagination):
     page_size = 3
+
+
+class FourPageNumberPagination(PageNumberPagination):
+    page_size = 4
