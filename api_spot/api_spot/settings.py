9531caf77254508ccf5730515a78ec3f20701262
arseny_wsl <arsenyxiii@gmail.com>
2023-10-13 11:48:53 +0300

    add inline

--- api_spot/api_spot/settings.py
+++ api_spot/api_spot/settings.py
@@ -11,7 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
-
 ALLOWED_HOSTS = (os.getenv('ALLOWED_HOSTS'), 'localhost', '127.0.0.1', '[::1]')
 
 
