4c94622a7488a69f80649f0c8a114816a6765b24
ZUS666 <t4g1.86@gmail.com>
2023-10-20 16:14:07 +0500

    fix resize

--- api_spot/spots/utils.py
+++ api_spot/spots/utils.py
@@ -1,11 +1,30 @@
-from PIL import Image, ImageOps
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage as storage 
+from django.core.files import File
+from pathlib import Path
+from PIL import Image
+from io import BytesIO
 
 
-def prepare_image(filepath: str, width: int, height: int) -> None:
-    """Обработка изображения перед сохранением в базу данных"""
-    img = Image.open(filepath)
-    img = ImageOps.exif_transpose(img)
-    new_image = img.resize((width, height))
-    if new_image.format != "JPEG":
-        new_image = new_image.convert("RGB")
-    new_image.save(filepath, "JPEG")
+def image_resize(image, width, height):
+    image_types = {
+        "jpg": "JPEG",
+        "jpeg": "JPEG",
+        "png": "PNG",
+        "gif": "GIF",
+        "tif": "TIFF",
+        "tiff": "TIFF",
+    }
+    img = Image.open(image)
+    if img.width > width or img.height > height:
+        img.thumbnail((width, height))
+        img_filename = Path(image.file.name).name
+        img_suffix = Path(image.file.name).name.split(".")[-1]
+        img_format = image_types[img_suffix]
+        buffer = BytesIO()
+        img.save(buffer, format=img_format)
+        print(img.width, img.height)
+        file_object = ContentFile(buffer.getvalue())
+        image.save(img_filename, file_object)
