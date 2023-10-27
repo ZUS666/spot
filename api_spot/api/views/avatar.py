86c04aa35a1b80a5eee0750707f3922fc5bc6f46
Arseny <arseny13@gmail.com>
2023-10-27 17:10:14 +0300

    isort

--- api_spot/api/views/avatar.py
+++ api_spot/api/views/avatar.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import (
-    extend_schema, OpenApiExample,
+    OpenApiExample, extend_schema,
 )
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api.mixins import UpdateDeleteViewSet
 from api.serializers.avatar import AvatarSerializer
