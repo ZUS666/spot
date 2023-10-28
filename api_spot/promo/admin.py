54617a679b2dfd9a795c822be4d3c953fc98eef3
ZUS666 <t4g1.86@gmail.com>
2023-10-28 06:20:33 +0500

    periodic

--- api_spot/promo/admin.py
+++ api_spot/promo/admin.py
@@ -6,6 +6,7 @@ from promo.models import EmailNews, Promocode
 @admin.register(Promocode)
 class PromcodeAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'name',
         'percent_discount',
         'expiry_date',
@@ -16,10 +17,18 @@ class PromcodeAdmin(admin.ModelAdmin):
 @admin.register(EmailNews)
 class EmailNewsAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'subject_message',
-        'send_date',
+        'send_datetime',
         'promocode',
         'is_sent',
         'text_message',
     )
     exclude = ('is_sent',)
+    readonly_fields = ('task',)
+
+    def delete_queryset(self, request, queryset):
+        for obj in queryset:
+            if obj.task:
+                obj.task.delete()
+        return super().delete_queryset(request, queryset)
--- api_spot/promo/services.py
+++ api_spot/promo/services.py
@@ -1,6 +1,9 @@
-from django.contrib.auth import get_user_model
+import json
 
-from promo.models import Promocode
+from django.contrib.auth import get_user_model
+from django_celery_beat.models import ClockedSchedule, PeriodicTask
+
+from promo.constants import NEWS_EMAIL_TEMPLATE, PROMOCE_EMAIL_TEMPLATE
 
 
 User = get_user_model()
@@ -24,14 +27,38 @@ def get_data_news(text_message):
     return context
 
 
-def get_data_promo(promocode_id):
+def get_data_promo(promocode):
     """
     Создание контекста для письма с промокодом.
     """
-    promocode = Promocode.objects.get(id=promocode_id)
     context = {
         'promocode_name': promocode.name,
         'percent_discount': promocode.percent_discount,
-        'expiry_date': promocode.expiry_date
+        'expiry_date': promocode.expiry_date.isoformat(),
     }
     return context
+
+
+def create_task_after_save_promo_email(obj):
+    """
+    Принимает объект письма
+    """
+    context = get_data_news(obj.text_message)
+    template = NEWS_EMAIL_TEMPLATE
+    if obj.promocode:
+        context = {**context, **get_data_promo(obj.promocode)}
+        template = PROMOCE_EMAIL_TEMPLATE
+    time = ClockedSchedule.objects.create(clocked_time=obj.send_datetime)
+    task = PeriodicTask.objects.create(
+        name=f'Email subject:{obj.subject_message} '
+             f'date:{obj.send_datetime.date().isoformat()}',
+        task='promo.tasks.create_chunk_task_send_mail',
+        clocked=time,
+        one_off=True,
+        args=json.dumps([
+            obj.subject_message,
+            template,
+            context,
+        ])
+    )
+    return task
